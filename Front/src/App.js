e0d82bef4922737e21106fc177149cd4dcbbf921
Alexandre Krunfli <alexandrekrunfli@MacBook-Pro-de-Alexandre.local>
2019-11-24 00:42:30 -0300

    subindo o inicio do front

---
+++ Front/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import TechList from './components/TechList'
+
+
+function App() {
+    return (<div>
+            <h1>Hello World!</h1>
+            <TechList />    
+        </div>
+    )
+}
+
+
+export default App;
